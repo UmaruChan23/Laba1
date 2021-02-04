49f3904845c3089c5a1256c7f7bb0ec8ef17c87d
UMRchan <f.matorin@mail.ru>
2021-02-04 12:11:38 +0300

    Task3 completed.

---
+++ Task3/task3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double calculate(double x);
+
+int main() {
+    double x = 5;
+    cout << "x = " << x << endl;
+    double result = calculate(x);
+    cout << round(result * 10000) / 10000 << endl;
+    cout << "Vvod x:";
+    cin >> x;
+    result = calculate(x);
+    cout << round(result * 10000) / 10000;
+    return 0;
+}
+
+double calculate(double x) {
+    return ((x + 2) / (sqrt(2 * x)) - (x / (sqrt(2 * x) + 2)) + (2 / (x - sqrt(2 * x)))) *
+           (sqrt(x) - sqrt(2)) / (x + 2);
+}
